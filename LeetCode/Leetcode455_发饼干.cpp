03aa66016be2a4e42eefcdf1bf66c062bbe72bff
CHOcho-quan <574166505@qq.com>
2021-05-23 16:42:53 +0800

    Re - Daily OJ

---
+++ LeetCode/Leetcode455_发饼干.cpp
@@ -0,0 +1,21 @@
+//
+//  Leetcode18_FourSum.cpp
+//  
+//
+//  Created by 铨 on 2019/1/20.
+//
+//
+
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        std::sort(g.begin(), g.end());
+        std::sort(s.begin(), s.end());
+        int child = 0, cookie = 0;
+        while (child < g.size() && cookie < s.size()) {
+            if (g[child] <= s[cookie]) ++child;
+            ++cookie;
+        }
+    return child;
+    }
+};
