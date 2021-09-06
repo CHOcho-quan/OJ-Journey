6eb75c9af9ee404eaaf66c01c3562ac9d3aa79b5
CHOcho-quan <574166505@qq.com>
2021-09-07 00:17:02 +0800

    daily

--- LeetCode/Leetcode1221_贪心分解平衡字符串.cpp
+++ LeetCode/Leetcode1221_贪心分解平衡字符串.cpp
@@ -1,30 +1,18 @@
-#include <iostream>
-#include <vector>
-#include <set>
-#include <stack>
-#include <algorithm>
-#include <cmath>
-
-using namespace std;
-
-int balancedStringSplit(string s) {
-    int l = 0, r = 0, re = 0;
-    
-    for (int i = 0;i < s.length();i++)
-    {
-        if (s[i] == 'L') l++;
-        if (s[i] == 'R') r++;
+class Solution {
+public:
+    int balancedStringSplit(string s) {
+        int re = 0;
+        unordered_map<char, int> check;
         
-        if (l == r && l != 0) {
-            re++;
-            l = 0;
-            r = 0;
+        for (int i = 0; i < s.length(); ++i) {
+            ++check[s[i]];
+            if (check['L'] == check['R'] && check['L'] != 0) {
+                ++re;
+                check['L'] = 0;
+                check['R'] = 0;
+            }
         }
+        
+        return re;
     }
-    return re;
-}
-
-int main() {
-    string s = "RLRRLLRLRL";
-    cout << balancedStringSplit(s);
-}
+};
--- LeetCode/Leetcode190_反转bit.cpp
+++ LeetCode/Leetcode190_反转bit.cpp
@@ -1,32 +1,11 @@
-//
-//  Leetcode190_反转bit.cpp
-//  
-//
-//  Created by 铨 on 2019/1/20.
-//
-//
-
-uint32_t GetIt(uint32_t n)
-{
-    uint32_t sum = 0;
-    while (n!=0)
-    {
-        n/=2;
-        sum++;
-    }
-    return sum;
-}
-
 class Solution {
 public:
     uint32_t reverseBits(uint32_t n) {
-        uint32_t a,sum = 0,t = GetIt(n);
-        for (int j = 0;j < t;j++)
-        {
-            a=n%2;
-            sum+=a*pow(2,31-j);
-            n/=2;
+        uint32_t re = 0, cnt = 31;
+        while (n != 0) {
+            re += n % 2 * pow(2, cnt--);
+            n /= 2;
         }
-        return sum;    
+        return re;
     }
 };
