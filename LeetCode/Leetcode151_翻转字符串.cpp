b0e9e0c5758d1933b65e979c9746245bfc62a337
CHOcho-quan <574166505@qq.com>
2022-07-17 20:22:01 -0700

    recent ojs

--- LeetCode/Leetcode151_翻转字符串.cpp
+++ LeetCode/Leetcode151_翻转字符串.cpp
@@ -1,27 +1,24 @@
-#include <iostream>
-#include <string>
-
-using namespace std;
-
-string reverseWords(string s) {
-    if (s.length()==0) return s;
-    string tmp = "", result = "";
-    int i = 0;
-    while (i < s.length()) {
-        if (s[i] == ' ') {i++;continue;}
-        tmp = "";
-        int j = i;
-        while (j < s.length() && s[j] != ' ')
-        {
-            tmp += s[j];
-            j++;
+class Solution {
+public:
+    string reverseWords(string s) {
+        string res = "", cur = "";
+        int cur_ind = 0;
+        while (cur_ind < s.length()) {
+            if (s[cur_ind] != ' ') {
+                cur = cur + s[cur_ind++];
+            } else {
+                ++cur_ind;
+                if (cur == "") continue;
+                else {
+                    res = ' ' + cur + res;
+                    cur = "";
+                }
+            }
         }
-
-        tmp += " ";
-        i = j + 1;
-        result = tmp + result;
+        
+        if (cur != "") res = cur + res;
+        if (res.length() > 0 && res[0] == ' ') res = res.substr(1, res.length());
+        
+        return res;
     }
-
-    if (result.length() != 0)result = string(result.begin(), result.end() - 1);
-    return result;
-}
+};
